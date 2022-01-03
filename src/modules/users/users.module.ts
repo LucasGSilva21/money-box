f47ee0775f86204a4cbb0f50be8056e503340ebb
LucasGSilva2102 <lucasgsilva2102@gmail.com>
2022-01-03 13:44:31 -0300

    feat: encrypt password before save user

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -3,10 +3,23 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { User, UserSchema } from './schemas/user.schema';
+import * as bcrypt from 'bcrypt';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeatureAsync([
+      {
+        name: User.name,
+        useFactory: () => {
+          const schema = UserSchema;
+          schema.pre('save', async function () {
+            const hash = await bcrypt.hash(this['password'], 10);
+            this['password'] = hash;
+          });
+          return schema;
+        },
+      },
+    ]),
   ],
   controllers: [UsersController],
   providers: [UsersService],
