996abb6ffff3420d0fb07cbfe8cfe0578c9a3c5f
Lucas Gabriel Silva <lucasgsilva2102@gmail.com>
2022-01-18 21:38:06 -0300

    feat: add mapper to find one user

---
+++ src/modules/users/dto/find-user.dto.ts
@@ -0,0 +1,29 @@
+import {
+  IsString,
+  MinLength,
+  MaxLength,
+  IsNotEmpty,
+  IsEmail,
+  IsMongoId,
+} from 'class-validator';
+import { Exclude, Expose } from 'class-transformer';
+
+@Exclude()
+export class FindUserDto {
+  @Expose()
+  @IsMongoId()
+  _id: string;
+
+  @Expose()
+  @IsString()
+  @MinLength(3)
+  @MaxLength(60)
+  @IsNotEmpty()
+  name: string;
+
+  @Expose()
+  @IsEmail()
+  @MaxLength(60)
+  @IsNotEmpty()
+  email: string;
+}
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -2,9 +2,9 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from './schemas/user.schema';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto, FindUserDto, UpdateUserDto } from './dto';
 import * as bcrypt from 'bcrypt';
+import { plainToClass } from 'class-transformer';
 
 @Injectable()
 export class UsersService {
@@ -32,8 +32,10 @@ export class UsersService {
     return this.userModel.find().exec();
   }
 
-  async findOne(id: string) {
-    return this.userModel.findById(id).exec();
+  async findOne(id: string): Promise<FindUserDto> {
+    const user = await this.userModel.findById(id).exec();
+
+    return plainToClass(FindUserDto, user);
   }
 
   async findByEmail(email: string) {
