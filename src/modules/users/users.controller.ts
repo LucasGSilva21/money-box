532b10a4f8357e73ac656771a63e9c4ad4c702a2
Lucas Gabriel <lucasgsilva2102@gmail.com>
2022-04-13 00:08:56 -0300

    feat: add swagger tag to user module

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -11,8 +11,10 @@ import {
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UpdateUserDto, FindUserDto } from './dto';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('users')
+@ApiTags('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
